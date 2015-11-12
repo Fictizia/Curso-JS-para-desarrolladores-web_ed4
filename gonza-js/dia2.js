3c591c2a24ceb543b15094e3a5f1f9c150ca1bc4
tritio <tritio2@gmail.com>
2015-11-12 19:25:10 +0000

    mi proyecto gonza

---
+++ gonza-js/dia2.js
@@ -0,0 +1,29 @@
+// ejercicio 1
+
+var x = 8;
+var y = 12; 
+console.log(x + " de " + y + " funcionando hoy");
+
+//ejercicio 3
+
+var x = 1 ;
+while(x<=8){
+    console.log("el tren número " + x + " está funcionando");
+    x++;
+}
+
+
+// ejercicio 4 
+
+var x = 1; 
+do {
+    console.log("el tren número " + x + " está funcionando");
+    x++;
+}while(x<9);
+
+// ejercicio 5
+
+for(var z=1; z<=8; z++) {
+    console.log( "el tren número " + z + " está funcionando");
+}
+
