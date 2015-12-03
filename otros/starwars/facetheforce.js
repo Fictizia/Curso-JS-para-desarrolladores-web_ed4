656dd5654698acf046cdf6c98cae0da0697cfd35
UlisesGascon <ulisesgascongonzalez@gmail.com>
2015-12-03 17:03:30 +0000

    Star wars

---
+++ otros/starwars/facetheforce.js
@@ -0,0 +1,90 @@
+var captainPhasma = {
+    name: "Capitán Phasma",
+    location: "Plaza Colón",
+    lat: 40.425232,
+    lon: -3.690493,
+    description: "La Capitán Phasma porta una distintiva armadura metálica y comanda las legiones de La Primera Orden.",
+    img: "phasma_big.png",
+    icon: "phasma.png"
+};
+
+var stormtrooper = {
+    name: "Soldado de asalto",
+    location: "Museo Reina Sofía",
+    lat: 40.408583,
+    lon: -3.693466,
+    description: "Los soldados de asalto son tropas de choque de élite extremamente leales al Imperio, por lo que resulta imposible desviarlos de la causa imperial. Visten imponentes armaduras blancas que cuentan con una amplia variedad de equipos de supervivencia y controles de temperatura que permiten a los soldados sobrevivir en casi cualquier entorno. Los soldados de asalto empuñan pistolas y rifles bláster con gran destreza, y atacaban en hordas para oprimir a sus enemigos. Junto a los soldados de asalto estándar, el Imperio organizó varias unidades especiales, incluidos los soldados exploradores y los soldados de las nieves.",
+    img: "stormtrooper_big.png",
+    icon: "stormtrooper.png"
+};
+
+var firstOrderStormtrooper = {
+    name: "Stormtrooper de La Primera Orden",
+    location: "Puerta de Alcalá",
+    lat: 40.419865,
+    lon: -3.688087,
+    description: "Equipados con una impecable armadura y poderosas armas, los Stormtroopers imponen la voluntad de La Primera Orden.",
+    img: "first_order_stormtrooper_big.png",
+    icon: "first_order_stormtrooper.png"
+};
+
+var resistancePilot = {
+    name: "Piloto de la Resistencia",
+    location: "Plaza España",
+    lat: 40.423486,
+    lon: -3.712074,
+    description: "Los pilotos de la Resistencia están bien entrenados y bien equipados, pero deben confiar en ataques relámpago cuando se enfrentan a la abrumadora superioridad de La Primera Orden.",
+    img: "resistance_pilot_big.png",
+    icon: "resistance_pilot.png"
+};
+
+var imperialPilot = {
+    name: "Piloto Imperial",
+    location: "Santa María de la Cabeza",
+    lat: 40.400988,
+    lon: -3.699544,
+    description: "Los pilotos imperiales confían en sus rápidos reflejos y su valentía para sobrevivir a las misiones contra los enemigos del Imperio. Los cazas imperiales disponían de un blindaje ligero y carecían de escudos. Solo los mejores pilotos se ganaron sus alas como expertos.",
+    img: "imperial_pilot_big.png",
+    icon: "imperial_pilot.png"
+};
+
+var rebelPilot = {
+    name: "Piloto Rebelde",
+    location: "Madrid Rio",
+    lat: 40.397563,
+    lon: -3.708626,
+    description: "Los pilotos son uno de los mejores activos de la Alianza Rebelde. Hay casi tantas historias como pilotos: los cazas rebeldes son pilotados por desertores del imperio, mercenarios reformados, granjeros en busca de aventuras y temerarios que sólo se sentían realmente vivos en una cabina de mando. Independientemente de sus orígenes, los pilotos rebeldes aprendieron a trabajar juntos, confiando en sus compañeros así como en su entrenamiento en incontables batallas contra el Imperio.",
+    img: "rebel_pilot_big.png",
+    icon: "rebel_pilot.png"
+};
+
+var darthVader = {
+    name: "Darth Vader",
+    location: "Glorieta de Bilbao",
+    lat: 40.429246,
+    lon: -3.702635,
+    description: "Darth Vader, quien fuera una vez un heroico Caballero Jedi, fue seducido por el lado oscuro de la Fuerza, se convirtió en un Lord Sith y condujo al imperio para destruir la Orden Jedi. Durante décadas, permaneció al servicio del Emperador, el malvado Darth Sidious, e hizo cumplir la voluntad de su Maestro tratando de aplastar a la incipiente Alianza Rebelde. Pero todavía había bondad en él...",
+    img: "darth_vader_big.png",
+    icon: "darth_vader.png"    
+};
+
+var firstOrderFlametrooper = {
+    name: "Flametrooper",
+    location: "Estadio Santiago Bernabéu",
+    lat: 40.452467,
+    lon: -3.689861,
+    description: "Los Flametroopers son soldados imperiales especializados de la Primer Orden. Portan armas incendiarias que pueden transformar cualquier campo de batalla en una llamarada infernal.",
+    img: "first_order_flametrooper_big.png",
+    icon: "first_order_flametroopers.png"    
+};
+
+var helmets = [
+    firstOrderFlametrooper, 
+    captainPhasma, 
+    firstOrderStormtrooper,
+    darthVader,
+    resistancePilot,
+    stormtrooper,
+    rebelPilot,
+    imperialPilot
+    ];
