9f8e37c6baba14b243f1860cf6f9c66f4ca3eadb
Kus Camara <kcmr@users.noreply.github.com>
2015-12-03 19:06:03 +0100

    ejercicio clase 7

---
+++ kus/clase-7/ajax.js
@@ -0,0 +1,33 @@
+// http://www.omdbapi.com/?t=hackers&y=&plot=short&r=json
+
+function ajaxRequest(url) {
+  var xmlHttp = new XMLHttpRequest();
+
+  // 0 uninitialized
+  // 1 loading
+  // 2 loaded
+  // 3 interactive
+  // 4 complete
+
+  xmlHttp.onreadystatechange = function() {
+    if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
+      console.info(JSON.parse(xmlHttp.responseText));
+    } else if (xmlHttp.readyState === 4 && xmlHttp.status === 400) {
+      console.error('ERROR 404');
+      console.info(JSON.parse(xmlHttp.responseText));
+    }
+  };
+
+  xmlHttp.open('GET', url, true);
+  xmlHttp.send();
+}
+
+// hackers
+console.log(ajaxRequest('http://www.omdbapi.com/?t=hackers&y=&plot=short&r=json'));
+
+// tiempo de Madrid, BCN y Valencia
+// 91854a050fca1d503656a021d3ab9169
+function getWeather(city) {
+  console.log(ajaxRequest('http://api.openweathermap.org/data/2.5/weather?q='+city+',es&units=metric&mode=json&appid=91854a050fca1d503656a021d3ab9169'));
+}
+getWeather('madrid');
