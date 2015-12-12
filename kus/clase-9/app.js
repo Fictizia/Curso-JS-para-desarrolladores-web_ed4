bf71ca77025d518ff4de641d55d5420b9ced1544
Kus Camara <kcmr@users.noreply.github.com>
2015-12-12 22:17:45 +0100

    ejercicio clase 9 agenda

--- kus/clase-9/app.js
+++ kus/clase-9/app.js
@@ -1,12 +1,62 @@
-(function() {
-  var form = document.querySelector('#form');
-  var contactList = document.querySelector('#contact-list');
+(function(w, d) {
+
+  var form = d.querySelector('#form');
+  var contactList = d.querySelector('#contact-list');
+  var noContacts = d.querySelector('#no-contacts');
+  var btnDeleteAll = d.querySelector('#btn-delete-all');
+  var keys = [];
+  var editMode = false;
 
   form.addEventListener('submit', saveContact);
-  if (localStorage.length) {
+  btnDeleteAll.addEventListener('click', deleteAllContacts);
+  d.addEventListener('contact-added', onContactAdded);
+  d.addEventListener('contact-deleted', onContactDeleted);
+
+  (function init() {
+    toggleNoContacts();
+
+    if (localStorage.length) {
+      setKeys();
+      setFormKey();
+      printContacts();
+    }
+  })();
+
+  w.editContact = function(el) {
+    setFormData(JSON.parse(localStorage.getItem(el.parentNode.dataset.key)));
+    editMode = true;
+  };
+
+  w.deleteContact = function(el) {
+    var parent = el.parentNode;
+    if (!confirm('¿Estás seguro de que quieres borrar a ' + parent.dataset.name + '?')) return;
+
+    localStorage.removeItem(el.parentNode.dataset.key);
+    keys[el.parentNode.dataset.key] = undefined;
     printContacts();
+
+    var contactDeleted = new CustomEvent('contact-deleted', { detail: { key: parent.dataset.key } });
+    d.dispatchEvent(contactDeleted);
+  };
+
+  function setKeys() {
+    for (var i = 0, len = localStorage.length; i < len; i++) {
+      if (localStorage.getItem(i)) {
+        keys[i] = i;
+      }
+    }
   }
 
+  function setFormKey() {
+    for (var i = 0, len = keys.length; i < len; i++) {
+      if (keys[i] === undefined) {
+        form.key.value = i;
+        return;
+      }
+    }
+
+    form.key.value = keys.length;
+  }
 
   function saveContact(e) {
     e.preventDefault();
@@ -14,74 +64,104 @@
     var data = {
       name: this.nombre.value,
       tel: this.tfno.value,
-      email: this.email.value
+      email: this.email.value,
+      key: this.key.value
     };
 
-    localStorage.setItem('key-' + data.name, JSON.stringify(data));
+    localStorage.setItem(data.key, JSON.stringify(data));
+    keys[data.key] = data.key;
 
-    printContacts();
+    if (editMode) {
+      printContacts();
+      editMode = false;
+    } else {
+      var contactAdded = new CustomEvent('contact-added', { detail: { data: data } });
+      d.dispatchEvent(contactAdded);
+    }
+
+    clearForm();
   }
 
-  function restoreFormData(key) {
-    var data = JSON.parse(localStorage.getItem('key-' + key));
+  function clearForm() {
+    form.nombre.focus();
+    form.nombre.value = '';
+    form.tfno.value = '';
+    form.email.value = '';
+    setFormKey();
+  }
+
+  function setFormData(data) {
     form.nombre.value = data.name;
     form.tfno.value = data.tel;
     form.email.value = data.email;
+    form.key.value = data.key;
     form.nombre.focus();
   }
 
   function printContacts() {
     contactList.innerHTML = '';
-    var buttons = '<button class="btn-edit">Editar</button><button class="btn-delete">Eliminar</button>';
 
     for (var key in localStorage) {
-      var data = JSON.parse(localStorage.getItem(key)),
-          li = document.createElement('li'),
-          divName = document.createElement('div'),
-          divTel = document.createElement('div'),
-          divEmail = document.createElement('div'),
-          editButtons = document.createElement('div');
-
-        editButtons.className = 'edit';
-        editButtons.setAttribute('data-name', data.name);
-        editButtons.innerHTML = buttons;
-        editButtons.addEventListener('click', onClickEdit);
-
-        divName.innerHTML = data.name;
-        divEmail.innerHTML = data.email;
-        divTel.innerHTML = data.tel;
-
-      li.appendChild(divName);
-      li.appendChild(divEmail);
-      li.appendChild(divTel);
-      li.appendChild(editButtons);
-
-      contactList.appendChild(li);
-    }
-
-    function onClickEdit(e) {
-      var dataName;
-      if (e.target.nodeName === 'BUTTON') {
-        dataName = e.target.parentNode.getAttribute('data-name');
-      } else {
-        dataName = e.target.getAttribute('data-name');
-      }
-
-      if (e.target.className === 'btn-edit') {
-        console.log('editar', dataName);
-        restoreFormData(dataName);
-        return;
-      }
-
-      if (e.target.className === 'btn-delete') {
-        console.log('borrar',dataName);
-        if (confirm('¿Seguro que deseas borrar a ' + dataName + '?')) {
-          localStorage.removeItem(dataName);
-          printContacts();
-        }
-
-        return;
-      }
+      var data = JSON.parse(localStorage.getItem(key));
+      printContact(data);
     }
   }
-})();
+
+  function printContact(data) {
+    var f = d.createDocumentFragment();
+
+    var buttons = [
+      '<button title="Editar" onclick="editContact(this)" class="btn btn--mini btn--edit"><i class="fa fa-pencil"></i></button>',
+      '<button title="Borrar" onclick="deleteContact(this)" class="btn btn--mini btn--delete"><i class="fa fa-trash"></i></button>'
+    ].join('');
+
+    var html = [
+    '<div class="data">',
+      '<div class="name">' + data.name + '</div>',
+      '<div class="email"><a href="mailto:' + data.email + '">' + data.email + '</a></div>',
+      '<div class="tel"><a href="tel:' + data.tel + '">' + data.tel + '</a></div>',
+    '</div>'
+    ].join('');
+
+    var li = d.createElement('li');
+    li.innerHTML = html;
+    f.appendChild(li);
+
+    var editButtons = d.createElement('div');
+    editButtons.className = 'edit';
+    editButtons.setAttribute('data-key', data.key);
+    editButtons.setAttribute('data-name', data.name);
+    editButtons.innerHTML = buttons;
+
+    li.appendChild(editButtons);
+    contactList.appendChild(f);
+  }
+
+  function toggleNoContacts() {
+    noContacts.classList.toggle('hidden', localStorage.length);
+    btnDeleteAll.classList.toggle('hidden', !localStorage.length);
+  }
+
+  function onContactAdded(e) {
+    toggleNoContacts();
+    printContact(e.detail.data);
+  }
+
+  function onContactDeleted(e) {
+    toggleNoContacts();
+    setFormKey();
+  }
+
+  function deleteAllContacts() {
+    if (!confirm('¿Estás seguro de que quieres borrar todos los contactos de tu agenda?')) return;
+
+    localStorage.clear();
+    keys = [];
+    toggleNoContacts();
+    printContacts();
+  }
+
+})(window, document);
+
+
+
